f45eae54c8a3af1c0f2a29ca241a866da71416b5
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-02-20 16:56:45 +0530

    Add files via upload

---
+++ natural.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n,sum;
+cout << "Enter the num: ";
+cin >> n;
+for(int i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+cout<<"The sum is "<<sum;
+return 0;
+}
